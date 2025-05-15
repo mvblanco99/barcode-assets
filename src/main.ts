bc6c008f5cba87a450bbaf60151fa570fa845bb3
jmboada30 <jmboada30@gmail.com>
2025-05-15 09:28:58 -0400

    feat: mejorar la función de generación de códigos de barras y ajustar el tamaño del lienzo

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,18 @@ if (!(canvas instanceof SVGSVGElement) || !canvas) {
 }
 const exportBtn = document.getElementById("export-btn") as HTMLButtonElement;
 
-// Esta funcion recibe dos argumentos obligatorios y uno opcional: 
+// Esta función recibe dos argumentos obligatorios y uno opcional:
 // 1. el codigo del articulo el cual es un entero
 // 2. cantidad de series a generar: es un entero que indica cuantas series de ese articulo se van a imprimir
 // 3. Indica desde que numero se empieza a contar las series, por defecto es 1
 // Ambos argumentos se concatenan para formar el codigo de barras
-// Ejemplo: si el articulo es 1 y la cantidad de series es 2, la funcion devuelve: 00001000001 y 00001000002 , donde los primeros 5 digitos son el articulo y los siguientes 6 son la serie
-function generateBarcode(article: number, series: number, start: number = 1): string[] {
+// Ejemplo: si el articulo es 1 y la cantidad de series es 2, la función devuelve: 00001000001 y 00001000002 , donde los primeros 5 digitos son el articulo y los siguientes 6 son la serie
+function generateBarcode(params: {
+  article: number;
+  series: number;
+  start: number;
+}): string[] {
+  const { article, series, start = 1 } = params;
   const barcodes: string[] = [];
   const articleStr = String(article).padStart(5, "0");
   for (let i = start; i < start + series; i++) {
@@ -29,8 +34,8 @@ function initCanvas() {
     throw new Error("Canvas element not found");
   }
   // definimos el tamaño real en mm
-  canvas.setAttribute("width", "1000mm");
-  canvas.setAttribute("height", "1000mm");
+  canvas.setAttribute("width", "1300mm");
+  canvas.setAttribute("height", "2350mm");
   // canvas.setAttribute("viewBox", "0 0 2000 2000");
 }
 
@@ -43,19 +48,29 @@ async function addBarcodes() {
   let x = 0;
   let y = 0;
 
-  const barcodeList = generateBarcode(1, 300, 1); // Genera 10 series del articulo 1
+  const barcodeList = generateBarcode({
+    article: 1,
+    series: 1000,
+    start: 1,
+  });
 
   for (const text of barcodeList) {
     // opts de Code128
     const opts: bwipjs.RenderOptions = {
       bcid: "code128",
-      textxalign: 'center',
-      textyoffset: 3,
-      text,
+      textxalign: "center",
+      textyoffset: 4,
+      text: text,
       width: 80, // mm
       height: 30, // mm
       includetext: true,
-      paddingtop: 10,
+      textsize: 12,
+      backgroundcolor: "#fff",
+      bordercolor: "#000",
+      borderwidth: 0.5,
+      borderbottom: 16,
+      bordertop: 8,
+      showborder: true,
     };
     // genera SVG string
     const svgStr = (bwipjs as any).toSVG(opts);
@@ -67,12 +82,12 @@ async function addBarcodes() {
     el.setAttribute("y", `${y}mm`);
     el.setAttribute("width", "85mm");
     el.setAttribute("height", "35mm");
-    el.setAttribute("style", "background-color: white;");
+    // el.setAttribute("border", "1px solid black");
 
     canvas.appendChild(el);
 
     x += pitchX;
-    if (x + 85 > 1000) {
+    if (x + 85 > 1300) {
       x = 0;
       y += pitchY;
     }
@@ -88,7 +103,7 @@ function exportSvg() {
   const url = URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.href = url;
-  link.download = "gigantografia.svg";
+  link.download = "barcode_articles.svg";
   link.click();
   URL.revokeObjectURL(url);
 }
